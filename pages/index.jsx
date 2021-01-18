5bcb296a413b79b40126cfd2114fdcc474515c19
dudenickoff <s.dudnikovv@gmail.com>
2021-01-18 14:02:35 +0200

    Fix assets path, fix favicon path, fix gh-pages static issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,12 @@ import Head from 'next/head';
 import { CurrentCity } from '../src';
 
 export default function Home() {
+  const faviconUrl = process.env.NODE_ENV === 'production' ? '/Weather_app/favicon.ico' : '/favicon.ico';
   return (
     <>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={faviconUrl} />
       </Head>
 
       <main className="grid grid--container">
--- src/constants/apiLinks.js
+++ src/constants/apiLinks.js
@@ -4,8 +4,7 @@ const IP_LOCATION_SERVICE_KEY = 'ad2efd6d679b54';
 export const MAIN_LINK = 'https://api.openweathermap.org/data/2.5/';
 export const FLAG_LINK = 'https://lipis.github.io/flag-icon-css/flags/4x3/';
 export const IP_LOC = `https://ipinfo.io/json?token=${IP_LOCATION_SERVICE_KEY}`;
-export const ICON_LINK = '/assets/';
-export const ASSETS = '../assets/';
-export const PROD_ASSETS = '/Weather/assets';
-export const PROD_ICON_LINK = '/Weather/assets/icons-png';
+// eslint-disable-next-line operator-linebreak
+export const ICON_LINK =
+  process.env.NODE_ENV === 'production' ? '/Weather_app/assets/' : '/assets/';
 export const UNITS = 'metric';
